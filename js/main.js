7dbcddbd887ccfdce47dac44297c97a3f3f5652d
Orieta Toro <orieta.toro@gmail.com>
2017-05-04 14:36:37 -0300

    mi scuad - inicio

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function scuad(nombre, apellido, edad, hobbies){
+	this.nombre = nombre,
+	this.apellido = apellido,
+	this.edad = edad,
+	this.hobbies = hobbies,
+	this.mostrarInfo = function(){
+		return(this.nombre + this.apellido + this.edad + this.hobbies)
+	}
+	const Rangi =new Rangi('Rangi', 'Becerra')
+	const Genesis = new Genesis('Genesis')
+	const Camila = new Camila('Camila', 'Gonzalez')
+	const Laura = new Laura('Laura', 'Pealez', 'Cinefila')
+	const Caterina = new Caterina('Caterina', 'DaSilva')
+	const Constanza = new Constanza('Constanza', 'Pecori' '26', 'tejer, comer y dormir')
+	const Rosita = new Rosita('Rosita', 'Diaz')
+	const Orieta = new Orieta('Orieta', 'Toro', '27', 'leer y dormir')
+}
+ document.write(this.nombre" " + apellido)
